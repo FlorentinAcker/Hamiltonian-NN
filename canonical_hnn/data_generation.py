589c160b5aa2a3fa14cb2e89d9bba66ad8412b9d
Florentin Acker <florentin.acker@polytechnique.edu>
2026-04-01 16:11:56 +0200

    add midpoint solver and simulate batch

--- canonical_hnn/data_generation.py
+++ canonical_hnn/data_generation.py
@@ -14,3 +14,26 @@ def derivatives(state):
     dth2 = (2*p2 - p1*d.cos()) / det    
     dp1 = p1*p2*d.sin()/det - N*d.sin()*d.cos()/det**2 - 2*th1.sin()
     dp2= p1*p2*d.sin()/det + N*d.sin()*d.cos()/det**2 - th2.sin()
+
+def iter_midpoint(state, dt, nb_iter=5):
+    z_mid = state.clone()
+    for i in range(nb_iter):
+        z_mid = state + dt/2*derivatives(z_mid)
+    return z_mid
+
+def step_midpoint(state, dt, nb_iter=5):
+    z_mid = iter_midpoint(state, dt, nb_iter=nb_iter)
+    return state + dt*derivatives(z_mid)
+
+def simulate_batch(z0, dt, T, nb_iter=5):
+    N_steps = int(T / dt)
+    states = torch.zeros(z0.shape[0], N_steps+1, 4)
+    states[0, :, 0] = z0 
+    for i in range(1, N_steps + 1):
+        states[:, i, :] = step_midpoint(states[:, i-1, :], dt=dt, nb_iter=nb_iter)
+        return states
+
+
+
+
+# %%
